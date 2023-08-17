80b4ab951863f8c640533b841c2b7c2b5f87b5d0
GuiPM001 <guilhermepimentelm0@gmail.com>
2023-08-16 23:45:11 -0300

    criar context Operações para conecatar com banco de dados

---
+++ Application/Context/OperacoesContext.cs
@@ -0,0 +1,15 @@
+﻿using Application.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Context
+{
+    public class OperacoesContext : DbContext
+    {
+        public DbSet<Operacao> OperacoesHistorico { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=master;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ Application/Model/Operacao.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Model
+{
+    public class Operacao
+    {
+        public Operacao(string nomeOperacao, double resultado, DateTime dataOperacao)
+        {
+            NomeOperacao = nomeOperacao;
+            Resultado = resultado;
+            DataOperacao = dataOperacao;
+        }
+
+        public int Id { get; set; }
+        public string NomeOperacao { get; set; }
+        public double Resultado { get; set; }
+        public DateTime DataOperacao { get; set; }
+    }
+}
