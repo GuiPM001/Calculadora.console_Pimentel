97a114cff529e62d1a6eeb895e4e82be5e1e9600
GuiPM001 <guilhermepimentelm0@gmail.com>
2023-08-16 23:45:27 -0300

    criar serviço para salvar as operações no arquivo de texto e banco de dados

---
+++ Application/Services/OperacaoService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Context;
+using Application.Model;
+
+namespace Application.Services
+{
+    public class OperacaoService
+    {
+        public static void Salvar(string nomeOperacao, float resultado, DateTime dataOperacao)
+        {
+            Console.WriteLine("Salvando dados...");
+
+            var operacao = new Operacao(nomeOperacao, resultado, DateTime.Now);
+
+            SalvarOperacaoNoBanco(operacao);
+            SalvarOperacaoNoArquivo(operacao);
+        }
+
+        private static void SalvarOperacaoNoBanco(Operacao operacao)
+        {
+            using var context = new OperacoesContext();
+
+            context.OperacoesHistorico.Add(operacao);
+            context.SaveChanges();
+        }
+
+        private static void SalvarOperacaoNoArquivo(Operacao operacao)
+        {
+            var caminhoArquivo = "C:\\Projetos\\Calculadora.console_Pimentel\\ultimo_comando.txt";
+            var conteudo = $"{operacao.NomeOperacao} - {operacao.DataOperacao}";
+
+            File.WriteAllText(caminhoArquivo, conteudo);
+        }
+    }
+}
